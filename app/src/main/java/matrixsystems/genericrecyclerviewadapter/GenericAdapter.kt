157ad53cfe54873fac8bc26291db0bbaf20cb190
Shahbaz Hashmi <shahbaz@DEV-386.local>
2019-10-17 13:18:54 +0530

    added generic adapter

--- app/src/main/java/matrixsystems/genericrecyclerviewadapter/GenericAdapter.kt
+++ app/src/main/java/matrixsystems/genericrecyclerviewadapter/GenericAdapter.kt
@@ -4,67 +4,48 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 /**
  * Created by Shahbaz Hashmi on 2019-10-17.
  */
-class GenericAdapter(context: Context, private val layout : Int, private val mData: List<String>) : RecyclerView.Adapter<GenericAdapter.ViewHolder>() {
+class GenericAdapter<T>(context: Context, private val layout : Int, private val mData: List<T>, private val mBinder : Binder<T>) : RecyclerView.Adapter<GenericAdapter<T>.ViewHolder>() {
 
     private val mInflater: LayoutInflater
-    private var mClickListener: ItemClickListener? = null
 
     init {
         this.mInflater = LayoutInflater.from(context)
     }
 
-    // inflates the row layout from xml when needed
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = mInflater.inflate(layout, parent, false)
         return ViewHolder(view)
     }
 
-    // binds the data to the TextView in each row
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val animal = mData[position]
-        holder.myTextView.setText(animal)
+        (holder as Binder<T>).bind(holder, position)
     }
 
-    // total number of rows
+
     override fun getItemCount(): Int {
         return mData.size
     }
 
 
-    // stores and recycles views as they are scrolled off screen
-    inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView),
-        View.OnClickListener {
-        internal var myTextView: TextView
-
-        init {
-            myTextView = itemView.findViewById(R.id.test_tv)
-            itemView.setOnClickListener(this)
-        }
-
-        override fun onClick(view: View) {
-            if (mClickListener != null) mClickListener!!.onItemClick(view, adapterPosition)
+    inner class ViewHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView), GenericAdapter.Binder<T>{
+        override fun bind(holder: ViewHolder, position: Int) {
+            mBinder.bind(holder, position)
         }
     }
 
-    // convenience method for getting data at click position
-    internal fun getItem(id: Int): String {
-        return mData[id]
+
+    interface Binder<T> {
+        fun bind(holder: GenericAdapter<T>.ViewHolder, position: Int)
     }
 
-    // allows clicks events to be caught
-    internal fun setClickListener(itemClickListener: ItemClickListener) {
-        this.mClickListener = itemClickListener
-    }
-
-    // parent activity will implement this method to respond to click events
-    interface ItemClickListener {
-        fun onItemClick(view: View, position: Int)
+    fun getItem(position : Int) : T {
+        return mData[position]
     }
 }
 
--- app/src/main/java/matrixsystems/genericrecyclerviewadapter/MainActivity.kt
+++ app/src/main/java/matrixsystems/genericrecyclerviewadapter/MainActivity.kt
@@ -2,42 +2,42 @@ package matrixsystems.genericrecyclerviewadapter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.LinearLayoutManager
 
 
-
-
-
-class MainActivity : AppCompatActivity() , GenericAdapter.ItemClickListener {
+class MainActivity : AppCompatActivity() {
 
     lateinit var recyclerView : RecyclerView
 
-    lateinit var genericAdapter: GenericAdapter
+    lateinit var genericAdapter: GenericAdapter<TestModel>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // data to populate the RecyclerView with
-        val animalNames = mutableListOf<String>()
-        animalNames.add("Horse")
-        animalNames.add("Cow")
-        animalNames.add("Camel")
-        animalNames.add("Sheep")
-        animalNames.add("Goat")
+        val animalList = mutableListOf<TestModel>()
+        animalList.add(TestModel("Horse"))
+        animalList.add(TestModel("Cow"))
+        animalList.add(TestModel("Camel"))
+        animalList.add(TestModel("Sheep"))
+        animalList.add(TestModel("Goat"))
 
         recyclerView = findViewById(R.id.recycler_view) as RecyclerView
         recyclerView.layoutManager = LinearLayoutManager(this)
-        genericAdapter = GenericAdapter(this, R.layout.row_test, animalNames)
-        genericAdapter.setClickListener(this)
+        genericAdapter = GenericAdapter(this, R.layout.row_test, animalList, object :  GenericAdapter.Binder<TestModel> {
+            override fun bind(holder: GenericAdapter<TestModel>.ViewHolder, position: Int) {
+                holder.itemView.findViewById<TextView>(R.id.test_tv).setText((genericAdapter.getItem(position) as TestModel).text)
+
+                holder.itemView.setOnClickListener {
+                    Toast.makeText(this@MainActivity, "You clicked " + (genericAdapter.getItem(position) as TestModel).text + " on row number " + position, Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
         recyclerView.adapter = genericAdapter
     }
 
-
-    override fun onItemClick(view: View, position: Int) {
-        Toast.makeText(this, "You clicked " + genericAdapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show()
-    }
 }
