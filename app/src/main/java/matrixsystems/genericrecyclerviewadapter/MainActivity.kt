f3ccf933d87ef9d468b42d4151b094524b72a2f6
Shahbaz Hashmi <shahbaz@DEV-386.local>
2019-10-17 14:42:39 +0530

    refactored adapter name

--- app/src/main/java/matrixsystems/genericrecyclerviewadapter/MainActivity.kt
+++ app/src/main/java/matrixsystems/genericrecyclerviewadapter/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var recyclerView : RecyclerView
 
-    lateinit var genericAdapter: GenericAdapter<TestModel>
+    lateinit var testAdapter: GenericAdapter<TestModel>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,16 +28,16 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView = findViewById(R.id.recycler_view) as RecyclerView
         recyclerView.layoutManager = LinearLayoutManager(this)
-        genericAdapter = GenericAdapter(this, R.layout.row_test, animalList, object :  GenericAdapter.Binder<TestModel> {
+        testAdapter = GenericAdapter(this, R.layout.row_test, animalList, object :  GenericAdapter.Binder<TestModel> {
             override fun bind(holder: GenericAdapter<TestModel>.ViewHolder, position: Int) {
-                holder.itemView.findViewById<TextView>(R.id.test_tv).setText((genericAdapter.getItem(position) as TestModel).text)
+                holder.itemView.findViewById<TextView>(R.id.test_tv).setText((testAdapter.getItem(position) as TestModel).text)
 
                 holder.itemView.setOnClickListener {
-                    Toast.makeText(this@MainActivity, "You clicked " + (genericAdapter.getItem(position) as TestModel).text + " on row number " + position, Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@MainActivity, "You clicked " + (testAdapter.getItem(position) as TestModel).text + " on row number " + position, Toast.LENGTH_SHORT).show()
                 }
             }
         })
-        recyclerView.adapter = genericAdapter
+        recyclerView.adapter = testAdapter
     }
 
 }
